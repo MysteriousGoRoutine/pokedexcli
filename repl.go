d2fe085be9d75c8d277fa2ed52e7f1fd2785b296
Igor <igor@nemkovich.dev>
2025-07-30 21:23:56 +0200

    Add basic REPL functionality and ignore log file

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,20 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		// Clean the input string
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
