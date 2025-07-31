35ee9be7ad368888629e730c7163a960cb9c7633
Igor <igor@nemkovich.dev>
2025-07-31 22:17:15 +0200

    Add help and exit command handlers

--- repl.go
+++ repl.go
@@ -7,26 +7,31 @@ import (
 	"strings"
 )
 
-var commands map[string]cliCommand
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
 
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
+		commandName := words[0]
 
-func commandHelp() error {
-	fmt.Println("Available commands:")
-	for _, command := range commands {
-		fmt.Printf("%s - %s\n", command.name, command.description)
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
-	return nil
 }
 
 func cleanInput(text string) []string {
@@ -35,43 +40,23 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func init() {
-	commands = map[string]cliCommand{
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
-		"help": {
-			name:        "help",
-			description: "Display help information",
-			callback:    commandHelp,
-		},
-	}
-}
-
-func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Println("Welcome to the Pokedex!")
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-
-		// Clean the input string
-		commands := cleanInput(scanner.Text())
-		if len(commands) == 0 {
-			continue
-		}
-		switch commands[0] {
-		case "exit":
-			commandExit()
-		case "help":
-			commandHelp()
-		default:
-			fmt.Println("Unknown command:", commands[0])
-		}
-		// fmt.Printf("Your command was: %s\n", words[0])
-
 	}
 }
