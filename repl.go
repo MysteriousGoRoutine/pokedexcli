2096905424bf4974dce724e6d964ca3fb781635d
Igor <igor@nemkovich.dev>
2025-07-31 20:51:33 +0200

    Add CLI command structure with help and exit commands

--- repl.go
+++ repl.go
@@ -7,24 +7,71 @@ import (
 	"strings"
 )
 
+var commands map[string]cliCommand
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Available commands:")
+	for _, command := range commands {
+		fmt.Printf("%s - %s\n", command.name, command.description)
+	}
+	return nil
+}
+
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
 }
 
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Display help information",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
+		fmt.Println("Welcome to the Pokedex!")
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 
 		// Clean the input string
-		words := cleanInput(scanner.Text())
-		if len(words) == 0 {
+		commands := cleanInput(scanner.Text())
+		if len(commands) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", words[0])
+		switch commands[0] {
+		case "exit":
+			commandExit()
+		case "help":
+			commandHelp()
+		default:
+			fmt.Println("Unknown command:", commands[0])
+		}
+		// fmt.Printf("Your command was: %s\n", words[0])
+
 	}
 }
